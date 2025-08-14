aafff733733d83db2a6e5804a50ace38267e4e8f
Steve McDougall <juststevemcd@gmail.com>
2025-08-14 10:19:18 +0100

    chore: basic structure created

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import alpinejs from '@astrojs/alpinejs';
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [alpinejs({ entrypoint: '/src/utils/alpine' })],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
---
+++ src/components/index.ts
@@ -0,0 +1,25 @@
+// Blocks
+import Logo from "./blocks/logo.astro";
+
+// Layout
+import Footer from "./layout/footer.astro";
+import Navigation from "./layout/navigation.astro";
+
+// UI
+import Container from "./ui/container.astro";
+import Section from "./ui/section.astro";
+import SkipLink from "./ui/skip-link.astro";
+
+export {
+    // Blocks
+    Logo,
+
+    // Layout
+    Footer,
+    Navigation,
+
+    // UI
+    Container,
+    Section,
+    SkipLink,
+};
