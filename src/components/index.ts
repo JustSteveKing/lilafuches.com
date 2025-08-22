0d607490e25534f905ab77beee984161adf5f99f
Steve McDougall <juststevemcd@gmail.com>
2025-08-22 15:06:21 +0100

    chore: fixing issues with components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,19 +1,26 @@
+// Blocks
+export { default as Logo } from "./blocks/Logo.astro";
+
 // Layout
 export { default as Footer } from "./layout/Footer.astro";
 export { default as Navigation } from "./layout/Navigation.astro";
 
 // UI Components
 export { default as ApiEndpointCard } from "./ui/ApiEndpointCard.astro";
+export { default as Breadcrumb } from "./ui/Breadcrumb.astro";
 export { default as Button } from "./ui/Button.astro";
 export { default as Card } from "./ui/Card.astro";
+export { default as ClientLogo } from "./ui/ClientLogo.astro";
 export { default as Container } from "./ui/Container.astro";
 export { default as Heading } from "./ui/Heading.astro";
+export { default as HighlightCard } from "./ui/HighlightCard.astro";
 export { default as ResourceCard } from "./ui/ResourceCard.astro";
 export { default as RotatingTestimonial } from "./ui/RotatingTestimonial.astro";
 export { default as Section } from "./ui/Section.astro";
 export { default as SkipLink } from "./ui/SkipLink.astro";
 export { default as StatsCard } from "./ui/StatsCard.astro";
 export { default as TeamMemberCard } from "./ui/TeamMemberCard.astro";
+export { default as Testimonial } from "./ui/Testimonial.astro";
 export { default as TestimonialGrid } from "./ui/TestimonialGrid.astro";
 export { default as Text } from "./ui/Text.astro";
 export { default as ThemeToggle } from "./ui/ThemeToggle.astro";
